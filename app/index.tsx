f05b21b18dda55057b70a4ff2acc7c5f4e79be2a
Nithya <appnithya.123@gmail.com>
2025-08-19 20:03:44 -0700

    Set up material for week 1 project meeting

--- app/index.tsx
+++ app/index.tsx
@@ -1,60 +1,15 @@
-import * as AuthSession from 'expo-auth-session';
-import { useRouter } from 'expo-router';
-import React, { useEffect, useState } from 'react';
-import { ActivityIndicator, Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
-
-// DEFINE CLIENT-SIDE CONSTANTS APPLICABLE TO ENTIRE APP
-const API_URL = 'http://192.168.2.59'; // this is your computer's IP address (the localhost of the server)
-const CLIENT_ID = '2e327947bec246929a9c902ab76e5172';
-const REDIRECT_URI = AuthSession.makeRedirectUri();
-
-const discovery = {
-  authorizationEndpoint: 'https://accounts.spotify.com/authorize',
-  tokenEndpoint: 'https://accounts.spotify.com/api/token', // not used directly
-};
-const router = useRouter();
+import React, { useState } from 'react';
+import { Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function HomeScreen() {
-  const [loading, setLoading] = useState(false);
+  /** useState("change me to Spotify!") creates a STATE VARIABLE called dummyText with an initial state. 
+   *  It also creates a setter function, called setDummyText, to update the state of dummyText.
+   */
+  const [dummyText, setDummyText] = useState("change me to Spotify!"); 
 
-  const [request, response, promptAsync] = AuthSession.useAuthRequest(
-    {
-      clientId: CLIENT_ID,
-      scopes: ['ugc-image-upload','app-remote-control', 'user-modify-playback-state','user-read-email', 'user-read-private', 'user-top-read', 'user-library-read', 'playlist-read-private', 'playlist-read-collaborative', 'playlist-modify-public', 'playlist-modify-private'],
-      redirectUri: REDIRECT_URI,
-      usePKCE: true,
-    },
-    discovery
-  );
-
-  useEffect(() => {
-    if (response?.type === 'success') {
-      const { code } = response.params;
-      exchangeCodeForToken(code, request?.codeVerifier!);
-    }
-  }, [response]);
-
-  const exchangeCodeForToken = async (code: string, codeVerifier: string) => {
-    setLoading(true);
-
-    try {  
-      const res = await fetch(`${API_URL}:3000/exchange`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ code, code_verifier: codeVerifier }),
-      });
-
-      const userData = await res.json();
-
-      router.navigate({
-        pathname: '/upload',
-        params: { userName: userData["display_name"], apiURL: API_URL},
-      });
-    } finally {
-      setLoading(false);
-    }
-
-  };
+  const dummyMethod = () => {
+    setDummyText("What should this be?");
+  }
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
@@ -68,18 +23,9 @@ export default function HomeScreen() {
         </View>
       </View>
 
-      {loading ? (
-        <>
-          <Text style={styles.labelText}>
-            loading...
-          </Text>
-          <ActivityIndicator size="large" color="#1DB954" style={{ marginTop: 20 }} />
-        </>
-      ) : (
-        <Pressable style={styles.button} onPress={() => promptAsync()}>
-          <Text style={styles.buttonText}>Log in with Spotify</Text>
-        </Pressable>
-      )}
+      <Pressable style={styles.button} onPress={() => dummyMethod()}>
+          <Text style={styles.buttonText}>Log in with {dummyText}</Text>
+      </Pressable>
     </ScrollView>
   );
 }
