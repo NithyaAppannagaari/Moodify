4239447e62eb1836c04a321b9fcc08d29006cb86
Nithya Appannagaari <nithya.app@berkeley.edu>
2025-10-03 18:26:04 -0700

    saving changes

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react';
 import { ActivityIndicator, Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 // DEFINE CLIENT-SIDE CONSTANTS APPLICABLE TO ENTIRE APP
-const API_URL = 'http://10.40.94.179'; // this is your computer's IP address (the localhost of the server) EDUROAM: exp://10.40.94.179:8081
+const API_URL = 'http://192.168.4.33'; // this is your computer's IP address (the localhost of the server) EDUROAM: exp://10.40.94.179:8081
 const CLIENT_ID = '2e327947bec246929a9c902ab76e5172';
 const REDIRECT_URI = AuthSession.makeRedirectUri();
 
